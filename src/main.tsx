6734eb3b97c2e20e143c2714afee437621e2a8da
amydojo <amycdo98@gmail.com>
2026-07-13 12:56:14 -0700

    Load v2 emotional POS styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import './styles.css'
 import './cvs-realism'
 import './styles/printer.css'
+import './styles/v2-layout.css'
+import './styles/v2-interactions.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
