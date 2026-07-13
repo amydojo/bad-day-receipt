da947adeb4eba63527e761e9e5c9ca61ac667c70
amydojo <amycdo98@gmail.com>
2026-07-13 13:13:53 -0700

    Load unified POS terminal styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './cvs-realism'
 import './styles/printer.css'
 import './styles/v2-layout.css'
 import './styles/v2-interactions.css'
+import './styles/terminal.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
