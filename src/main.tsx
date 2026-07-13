7d2f911194e6d071830e447de4f850bd449d2472
amydojo <amycdo98@gmail.com>
2026-07-13 13:50:07 -0700

    Load finished receipt shadow refinement

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './styles/v2-layout.css'
 import './styles/v2-interactions.css'
 import './styles/terminal.css'
 import './styles/slot-depth.css'
+import './styles/slot-finish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
