294f964b0f20adc150ca253bc107d1398fef657d
amydojo <amycdo98@gmail.com>
2026-07-13 18:02:34 -0700

    Load Soft Machine shell foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { SoftMachineShell } from './soft-machine/SoftMachineShell'
 import './styles.css'
 import './cvs-realism'
 import './styles/printer.css'
@@ -9,9 +10,12 @@ import './styles/v2-interactions.css'
 import './styles/terminal.css'
 import './styles/slot-depth.css'
 import './styles/slot-finish.css'
+import './soft-machine/softMachine.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SoftMachineShell machineId="bad-day-receipt" phase="idle">
+      <App />
+    </SoftMachineShell>
   </StrictMode>,
 )
