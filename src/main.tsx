e3a544a483dde3fc2406ab64a224a805ac0fc7ac
amydojo <amycdo98@gmail.com>
2026-07-13 18:32:13 -0700

    Load accessibility safeguards last

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './soft-machine/machineStage.css'
 import './soft-machine/artifactActions.css'
 import './soft-machine/bottomSheet.css'
 import './soft-machine/pwa.css'
+import './soft-machine/accessibility.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
